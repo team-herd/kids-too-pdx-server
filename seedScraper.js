a8e4fbae7f5855dedee88fb77204b03e0cc58548
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-26 08:31:11 -0700

    days working

--- seedScraper.js
+++ seedScraper.js
@@ -1,10 +1,8 @@
 
-const eventScraper = require('./lib/services/saScraper');
-const parks = require('./lib/services/parkSwimScraper');
-// const library = require('./lib/services/libraryScraper');
+const library = require('./lib/services/libraryScraper');
 const pdxparent = require('./lib/services/pdxParent');
 
-pdxparent()
-  .then(events => {
-    
-  });
+pdxparent();
+library();
+
+
