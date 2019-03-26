7d328fbaaea83372b9ceceb77b62029e4bc2aefe
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-26 11:11:51 -0700

    working on data

--- seed.js
+++ seed.js
@@ -1,9 +1,16 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
 const mongoose = require('mongoose');
-const seedData = require('./tests/seedData');
+// const seedData = require('./tests/seedData');
+const art = require('./lib/utils/artsParser');
 
-seedData(100)
-  .then(() => console.log('done'))
-  .catch(err => console.error(err))
+
+art({})
+  .then(() => console.log('Done'))
+  .catch(err=> console.error(err))
   .finally(() => mongoose.connection.close());
+
+// seedData(100)
+//   .then(() => console.log('done'))
+//   .catch(err => console.error(err))
+//   .finally(() => mongoose.connection.close());
