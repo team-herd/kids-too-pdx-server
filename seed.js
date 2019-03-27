119b5ce1d5f22e93aaea0d2f33776f2a4ea7f6ed
Dyanna Tolman <dyannast@gmail.com>
2019-03-27 10:49:40 -0700

    new seed.js

--- seed.js
+++ seed.js
@@ -1,27 +1,54 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
 const mongoose = require('mongoose');
-const data = require('./lib/utils/csvParser');
+const dataSA = require('./lib/utils/csvParser');
 const art = require('./lib/utils/artsParser');
 const library = require('./lib/services/libraryScraper');
 const pdxparent = require('./lib/services/pdxParent');
+const { getParksAndRec } = require('./lib/services/parksAndRec');
+const { getNonProfit } = require('./lib/services/nonProfit');
 
-art({})
-  .then(() => console.log('Done'))
-  .catch(err => console.error(err))
-  .catch(err=> console.error(err))
-
-data({})
-  .then(() => console.log('done'))
-  .catch(err => console.error(err))
-  .finally(() => mongoose.connection.close());
-
-pdxparent({})
-  .then(() => console.log('done'))
-  .catch(err => console.error(err))
-  .finally(() => mongoose.connection.close());
-
-library({})
-  .then(() => console.log('done'))
+Promise.all([
+  art(), 
+  dataSA(),
+  pdxparent(),
+  library(),
+  getParksAndRec(),
+  getNonProfit()
+])
+  .then(()=>console.log('done'))
   .catch(err => console.error(err))
   .finally(() => mongoose.connection.close());
+  
+// art({})
+//   .then(() => console.log('Done ART'))
+//   .then(dataSA)
+//   .then(() => console.log('done SA'))
+//   .then(pdxparent)
+//   .then(() => console.log('DONE PDX'))
+
+// dataSA({})
+//   .catch(err => console.error(err))
+//   .finally(() => mongoose.connection.close());
+
+// pdxparent({})
+//   .catch(err => console.error(err))
+//   .finally(() => mongoose.connection.close());
+
+// library({})
+//   .then(() => console.log('DONE LIBRARY'))
+//   .catch(err => console.error(err))
+//   .finally(() => mongoose.connection.close());
+
+// getParksAndRec({})
+//   .then(() => console.log('DONE PARKS '))
+//   .catch(err => console.error(err))
+//   .finally(() => mongoose.connection.close());
+
+
+// getNonProfit({})
+//   .then(() => console.log('DONE PROFIT'))
+//   .catch(err => console.error(err))
+//   .finally(() => mongoose.connection.close());
+
+
