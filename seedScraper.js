6842f16cc7bc5c17f79530ad242b91e83bd0cf2c
Shaba9 <shaba92@gmail.com>
2019-03-27 12:39:19 -0700

    data files organized in services

--- seedScraper.js
+++ seedScraper.js
@@ -1,6 +1,2 @@
-// const eventScraper = require('./lib/services/saturdayAcademy');
-const library = require('./lib/services/libraryScraper');
-// const pdxparent = require('./lib/services/pdxParent');
-
-// pdxparent();
+const library = require('./lib/services/data/scrapers/libraryScraper');
 library();
